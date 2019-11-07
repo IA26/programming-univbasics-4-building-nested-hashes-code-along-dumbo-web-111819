9c97a0277fb9ef89c4c7ddc453cf1dbb42aa12d3
Isaac Avilez <isaac1180@aol.com>
2019-11-07 18:22:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,19 +3,17 @@ def third_challenge
     :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-<<<<<<< HEAD
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-=======
+
       :hero => {name: "Romoe", age: "15", status: "alive"},
       :hero_friends => []
       }, 
     :capulet => {
       :patriarch => {name: "Romeo", age: "50"},
->>>>>>> 489a675563d17cfbee47e273932df80cefcb7df7
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
